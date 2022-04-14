695b4b8e327ed1f5a7c76f6c23a6a0d3264b7b98
WhySoFast <matheusvca@gmail.com>
2022-04-14 01:16:16 -0300

    criando app de produtos

---
+++ product-api/src/main/kotlin/com/coursemsc/product/MicrosservicesCommunicationApplication.kt
@@ -0,0 +1,11 @@
+package com.coursemsc.product
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicrosservicesCommunicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<MicrosservicesCommunicationApplication>(*args)
+}
---
+++ product-api/src/test/kotlin/com/coursemsc/product/MicrosservicesCommunicationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coursemsc.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicrosservicesCommunicationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
